51f3c07ca4614d43ce8f5891d7aad3aee9531984
Abdul Fater <abdulfater410@gmai.com>
2025-04-22 20:05:28 +0530

    understanding how to create custom react elements and their flow

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,13 @@
 import Abdul from "./Abdul"
 
 function App() {
+
+  const username = "Abdul Fater"
+
   return (
     <>
     
-    <h1>Rendering First Tag</h1>
+    <h1>Rendering First Tag by {username} </h1>
     <h2>Rendering Third Tag</h2>
     <Abdul/>
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import App from './App.jsx';  
+
+ export function MyApp(){
+
+      return (
+            <div>
+                  <h1>Custom React App</h1>
+            </div>
+      )
+}
+
+// const ReactElement = {
+//       type : 'a',
+  
+//       props : {
+//           href : 'https://google.com',
+//           target : '_blank'
+//       },
+  
+//       children : 'Click me to visit Google'
+//   }
+
+//   const anotherElement = (
+//       <a href="https://google.com" target='_blank'>Visit to Google</a>
+//   )
+
+  const ReactElement = React.createElement(
+      'a',
+      {href: 'https://google.com' , target:'_blank'},
+      'click here to google ok na !'
+  )
+
 
 createRoot(document.getElementById('root')).render(
   
-      <App />
+      <App/> 
   
 )
