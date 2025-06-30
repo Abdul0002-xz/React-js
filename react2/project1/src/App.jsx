16c0eb6eaa350a245ce054831bc7068897c10e2c
Abdul Fater <abdulfater410@gmai.com>
2025-06-30 21:57:49 +0530

    Starting Again

---
+++ react2/project1/src/App.jsx
@@ -0,0 +1,16 @@
+import KgButton from "./Components/KgButton"
+
+function App(){
+
+  return <div>
+     <h1>
+    I am the best person who is learning reactjs
+     </h1>
+
+     <KgButton/>
+
+  </div>
+
+}
+
+export default App
