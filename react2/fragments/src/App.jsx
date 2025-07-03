e09269be662083d1f514ea6b6dfa5684285f5d16
Abdul Fater <abdulfater410@gmai.com>
2025-07-03 23:10:40 +0530

    done conditional formatting

--- react2/fragments/src/App.jsx
+++ react2/fragments/src/App.jsx
@@ -1,10 +1,15 @@
 function App() {
 
   let foodItems = ["Dal" , "Roti" , "Green Vegatables" , "Fruits" , "Salad" , "Milk" , "Ghee"];
+  // let foodItems = [];
+
+  
 
   return (
     <>
       <h1>Healthy Foods</h1>
+      {/* {foodItems.length === 0 ? <h3>I am still hungry</h3> : null } */}
+      {foodItems.length === 0 && <h3>I am still hungry</h3> }
       <ul className="list-group">
         {foodItems.map((item)=>(
           <li key={item} className="list-group-item">{item}</li>
