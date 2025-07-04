43a290068d3630280f4293fb04368f55211dcbe6
Abdul Fater <abdulfater410@gmai.com>
2025-07-05 00:25:59 +0530

    learned how to divide normal code into components

---
+++ react2/project4/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Display from "./Components/display";
+import ButtonContainer from "./Components/ButtonsContainer";
+
+function App() {
+  return (
+    <div className="outer-container">
+      <Display />
+      <ButtonContainer />
+    </div>
+  );
+}
+
+export default App;
