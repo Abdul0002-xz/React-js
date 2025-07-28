d016571fc9cb99876a4973dbe09dee4e0394c177
Abdul Fater <abdulfater410@gmai.com>
2025-07-28 23:24:08 +0530

    writing code with appWrite

--- 13MegaBlog/src/appwrite/Auth.js
+++ 13MegaBlog/src/appwrite/Auth.js
@@ -1,17 +1,50 @@
 import config from "../Config/Config";
-import {Client , Account , ID} from "appwrite";
-class Auth{
+import { Client, Account, ID } from "appwrite";
+class Auth {
     Client = new Client();
     Account;
 
-    constructor(){
+    constructor() {
         this.Client
             .setEndpoint(config.appwriteUrl)
             .setProject(config.appwriteProjectId);
-            
-            this.Account = new Account(this.Client)
+
+        this.Account = new Account(this.Client)
     }
+
+
+    async createAccount({ email, password, name }) {
+        try {
+
+            const userAccount = await this.Account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                // wre will call another callback function
+                return this.Login({email,password});
+            }
+
+            else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            console.error("Account creation failed:", error.message);
+            throw new Error("Unable to create account. Please try again.");
+        }
+    }
+
+    async Login({ email, password }) {
+        try {
+            return this.Account.createEmailPasswordSession(email, password);
+        }
+        catch (error) {
+            console.error("Account Login failed:", error.message);
+            throw new Error("Unable to Login account. Please try again.");
+        }
+    }
+
     
+
 
 };
 
